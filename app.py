ab745d56522e3cadf2a29d75e3a581356e9f4480
Robert Saula Mota <robertsaulamota@gmail.com>
2025-07-10 14:45:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -68,4 +68,4 @@ def export_excel():
     return send_file(output, download_name='lego_scanned.xlsx', as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000)
